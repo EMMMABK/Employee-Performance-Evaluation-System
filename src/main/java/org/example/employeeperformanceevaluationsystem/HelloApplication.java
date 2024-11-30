3c2f23ded6cfa840798edb3f458f6cb5d63de017
EMMMABK <adilbikiev2@gmail.com>
2024-11-30 20:15:39 +0600

    add hope

--- src/main/java/org/example/employeeperformanceevaluationsystem/HelloApplication.java
+++ src/main/java/org/example/employeeperformanceevaluationsystem/HelloApplication.java
@@ -2,7 +2,6 @@ package org.example.employeeperformanceevaluationsystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,24 +9,14 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Загружаем FXML файл
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        // Инициализируем EmployeeApp
+        EmployeeApp employeeApp = new EmployeeApp();
 
-        // Загружаем корневой элемент из FXML
-        Parent root = loader.load();
-
-        // Создаем сцену с корневым элементом
-        Scene scene = new Scene(root);
-
-        // Устанавливаем сцену в окно
-        primaryStage.setScene(scene);
-
-        // Настроим заголовок окна и показываем его
-        primaryStage.setTitle("Employee Performance Evaluation");
-        primaryStage.show();
+        // Запускаем EmployeeApp и передаем primaryStage
+        employeeApp.start(primaryStage);
     }
 
     public static void main(String[] args) {
-        launch(args);  // Запуск приложения
+        launch(args); // Запуск JavaFX приложения
     }
 }
