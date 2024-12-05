3750b4882707b30366c7d98a08d38aae6cab2b62
Altynbek19 <altynbek.zhonguchkaev@alatoo.edu.kg>
2024-12-05 19:31:06 +0600

    sql

--- sql/Employee-Performance-Evaluation-System.sql
+++ sql/Employee-Performance-Evaluation-System.sql
@@ -2,8 +2,21 @@ CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     department VARCHAR(50) NOT NULL,
-    hire_date DATE NOT null
+    hire_date DATE NOT NULL
 );
+
+
+CREATE TABLE grades (
+    id SERIAL PRIMARY KEY,
+    employee_id INT REFERENCES employees(id) ON DELETE NO ACTION,
+    attendance INT NOT NULL,
+    softskill INT NOT NULL,
+    hardskill INT NOT NULL,
+    grade FLOAT DEFAULT 0
+);
+ALTER TABLE grades DROP CONSTRAINT grades_employee_id_fkey;
+ALTER TABLE grades ADD CONSTRAINT unique_employee_id UNIQUE (employee_id);
+
 
 CREATE TABLE trash (
     id SERIAL PRIMARY KEY,
@@ -13,16 +26,4 @@ CREATE TABLE trash (
     hire_date DATE,
     deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 ALTER TABLE trash DROP CONSTRAINT trash_employee_id_fkey;
-CREATE TABLE performance_reviews (
-    id SERIAL PRIMARY KEY,  -- айди самой таблицы
-    employee_id INT NOT NULL REFERENCES employees(id) ON DELETE CASCADE,  -- айди сотрудника (с удалением записи из performance_reviews при удалении сотрудника)
-    review_date DATE NOT NULL,  -- дата оценки
-    performance_score DOUBLE PRECISION CHECK (performance_score BETWEEN 1 AND 10),  -- средний балл от 1 до 10
-    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE  -- внешний ключ с каскадным удалением
-);
-
-
-
-
