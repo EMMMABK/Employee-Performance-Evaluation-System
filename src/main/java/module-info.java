bfd98d464b87b3f83e94f469c4e351363eb9696d
EMMMABK <adilbikiev2@gmail.com>
2024-12-16 16:12:16 +0600

    add Validation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.employeeperformanceapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.postgresql.jdbc;
 
 
     opens org.example.employeeperformanceapp to javafx.fxml;
